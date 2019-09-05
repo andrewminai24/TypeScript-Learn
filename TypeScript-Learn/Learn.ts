eb574359e715da65c867707179c7373283835c53
andrewminai24 <andrewminai24@gmail.com>
2019-09-05 11:30:50 -0700

    typescript learn

---
+++ TypeScript-Learn/Learn.ts
@@ -0,0 +1,24 @@
+/*
+Here is what TypeScript add from JavaScript
+classes
+private attributes
+closures(With fat arrow syntax and intuitive variable scope)
+decorators(roughly comparable to javas annotations)
+getters and setters
+
+None of these concepts are new to JavaScript. 
+They are emulated using functions. 
+But that's the catch: JavaScript uses a single idea -
+ functions - to implement a variety of different concepts. 
+ TypeScript uses dedicated keywords. 
+This makes TypeScript code a pleasant read. 
+
+Java forces you to declare trivial facts over and over again.
+It doesnt 
+
+*/
+
+//Java 7 Map<String,String> trafficLights = new HashMap<>();
+
+
+let trafficLights = new Map<string,string>();
